c4aac26ab5d21c5b2e418ae0073d425068c897ff
whes1015 <44525760+whes1015@users.noreply.github.com>
2022-04-16 14:30:22 +0800

    Update config.js

--- Discord_JavaScript_Bot/config.js
+++ Discord_JavaScript_Bot/config.js
@@ -1,7 +1,8 @@
-const Token="OTIyODA3OTQ0NDMxNjk3OTMw.YcG13g.JVdwoZz7eOBHx1feFZemCM0okwo"
+const Token=""
 const API_POST="http://150.117.110.118:10150"
 const API_WebSocket="ws://150.117.110.118:910"
-const APIkey="a5ef9cb2cf9b0c86b6ba71d0fc39e329"
+const APIkey=""
+
 //到 main.js 的路徑
 const Path="C:/Users/whes1015/Desktop/ExpTech_Bot/"
 
